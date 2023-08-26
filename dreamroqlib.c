e0b321093f819d9c4f026acf95f757dfa4d2a71a
Ian Micheal <59771322+ianmicheal@users.noreply.github.com>
2023-08-26 10:22:47 -0400

    Add files via upload
    aligned frame for pvr dma

--- dreamroqlib.c
+++ dreamroqlib.c
@@ -14,6 +14,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <malloc.h>
 #include <dc/fmath_base.h>
 #include <kos/fs.h> // Include the KOS filesystem header
 #include "dreamroqlib.h"
@@ -30,7 +31,7 @@
 #define LE_16(buf) (*buf | (*(buf+1) << 8))
 #define LE_32(buf) (*buf | (*(buf+1) << 8) | (*(buf+2) << 16) | (*(buf+3) << 24))
 
-#define MAX_BUF_SIZE (64 * 1024)
+#define MAX_BUF_SIZE (64 * 2048)
 
 
 #define ROQ_CODEBOOK_SIZE 256
@@ -409,7 +410,7 @@ int dreamroq_play(char *filename, int loop, render_callback render_cb,
         return ROQ_FILE_READ_FAILURE;
     }
     framerate = LE_16(&read_buffer[6]);
-   printf("RoQ file plays at %d frames/sec\n", framerate);
+  // printf("RoQ file plays at %d frames/sec\n", framerate);
 	
     /* Initialize Audio SQRT Look-Up Table */
     for(i = 0; i < 128; i++)
@@ -456,9 +457,7 @@ while (1)
     {
         status = ROQ_FILE_READ_FAILURE;
         break;
-    }
-
-            
+    }        
         switch(chunk_id)
         {
         case RoQ_INFO:
@@ -495,8 +494,8 @@ while (1)
             printf("  RoQ_INFO: dimensions = %dx%d, %dx%d; %d mbs, texture = %dx%d\n", 
                 state.width, state.height, state.mb_width, state.mb_height,
                 state.mb_count, state.stride, state.texture_height);
-            state.frame[0] = (unsigned short*)malloc(state.texture_height * state.stride * sizeof(unsigned short));
-            state.frame[1] = (unsigned short*)malloc(state.texture_height * state.stride * sizeof(unsigned short));
+            state.frame[0] = (unsigned short*)memalign(32, state.texture_height * state.stride * sizeof(unsigned short));
+            state.frame[1] = (unsigned short*)memalign(32, state.texture_height * state.stride * sizeof(unsigned short));
             state.current_frame = 0;
             if (!state.frame[0] || !state.frame[1])
             {
@@ -569,4 +568,5 @@ while (1)
 
     return status;
 }
+
 
